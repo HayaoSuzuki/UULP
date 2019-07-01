9dee8dc2076188c32ed1597e0c54d38b496e0de3
Hayao Suzuki <hayao.suzuki@iridge.jp>
2019-07-01 16:54:04 +0900

    一応動くRust版more01の実装
    エラー処理がクソ雑魚なのと、標準入力のmoreの場合ができていない。

--- chap01/more01.rs
+++ chap01/more01.rs
@@ -1,19 +1,56 @@
-use std::env;
-use std::io;
+use std::io::BufRead;
+
+const PAGELEN: i32 = 24;
 
 fn main() {
-    let _args: Vec<String> = env::args().collect();
+    let mut _args = Vec::new();
 
-    if *&_args[1..].len() == 0 {
-        // do_more(io::stdin())
-        println!("arg = {}", "arg");
+    for arg in std::env::args().skip(1) {
+        _args.push(arg);
+    }
+
+    if _args.len() == 0 {
+        println!("hoge");
+    // do_more(std::io::stdin);
     } else {
-        for arg in &_args[1..] {
-            println!("arg = {}", arg);
+        for arg in &_args {
+            do_more(std::io::BufReader::new(std::fs::File::open(arg).unwrap()));
         }
     }
 }
 
-fn do_more() {}
+fn do_more<R>(reader: R) -> std::io::Result<()>
+where
+    R: BufRead,
+{
+    let mut num_of_lines = 0;
+    for line_result in reader.lines() {
+        let line = line_result?;
+        if num_of_lines == PAGELEN {
+            let reply = see_more();
+            if reply == 0 {
+                break;
+            }
+            num_of_lines -= reply
+        }
+        println!("{}", line);
+        num_of_lines += 1;
+    }
+    Ok(())
+}
 
-fn see_more() {}
+fn see_more() -> i32 {
+    println!("more?");
+    let mut c = String::new();
+    std::io::stdin().read_line(&mut c).ok().expect("Failed");
+    if c == "q\n" {
+        return 0;
+    }
+    if c == " \n" {
+        return PAGELEN;
+    }
+    if c == "\n" {
+        return 1;
+    }
+    return 0;
+}
